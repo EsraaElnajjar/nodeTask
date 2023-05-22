c398676b637f7cad7deacf5142364d6cef23fcec
israaelnajjar <israaelnajjar664@gmail.com>
2023-05-22 16:11:52 +0300

    first commit

--- train2.js
+++ train2.js
@@ -1,29 +1,30 @@
 import express from 'express'
-const app=express();
+const app = express();
 app.listen(700);
-const students =[
+const students = [
     {
-     id:1,
-     name:"esraa"
-    },{
-        id:2,
-        name:"alaa"
-    },{
-        id:3,
-        name:"aya"
+        id: 1,
+        name: "esraa"
+    }, {
+        id: 2,
+        name: "alaa"
+    }, {
+        id: 3,
+        name: "aya"
     },
     {
-        id:4,
-        name:"aliaa"
+        id: 4,
+        name: "aliaa"
     }
 ];
-app.get('/student',(req,res)=>{
-    let output="<ul>";
-  for(let i=0;i<students.length;i++){
-    const student=students[i];
-    output+="<li>"+ student.name +"</li>"
-  }
-    output+="</ul>";
+app.get('/student', (req, res) => {
+    let output = "<ul>";
+    for (let i = 0; i < students.length; i++) {
+        const student = students[i];
+        output += "<li><a href='/student/" + student.id + "'>" + student.name + "</a></li>";
+    }
+    output += "</ul>";
     res.send(output);
+    
 });
 
